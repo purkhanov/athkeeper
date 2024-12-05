40707b17068ad034b016930d75622da1c41c7dee
Nurlan Purkhanov <ppurkhanov@gmail.com>
2024-12-05 17:09:33 +0500

    activated log settings in main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
+import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .users.router import router as user_router
-from .auth.router import router as auth_router
-from .config import  ORIGINS
+from src.config import settings
 
 
 app = FastAPI()
-
+settings.logging.configure_logging(level=logging.INFO)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ORIGINS,
+    allow_origins = settings.cors.ORIGINS,
     allow_credentials = True,
-    allow_methods = ['*'],
-    allow_headers = ['*']
+    allow_methods = settings.cors.ALLOW_METHODS,
+    allow_headers = settings.cors.ALLOW_HEADERS,
 )
 
-app.include_router(auth_router)
 app.include_router(user_router)
