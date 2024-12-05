b57c1a379aeffe57037c13f23fd7ded0fbc469c0
Nurlan Purkhanov <ppurkhanov@gmail.com>
2024-12-05 17:13:08 +0500

    changed models

--- src/database/models.py
+++ src/database/models.py
@@ -1,29 +1,40 @@
 from datetime import datetime
+from enum import Enum
 from sqlalchemy import String, BigInteger, func
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 
 
+class Role(Enum):
+    user = 'user'
+    admin = 'admin'
+
+
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(primary_key = True)
-    created_at: Mapped[datetime] = mapped_column(server_default = func.now(), default = datetime.now())
+    created_at: Mapped[datetime] = mapped_column(
+        server_default = func.now(),
+        default = datetime.now(),
+    )
 
 
 class User(Base):
     __tablename__ = "users"
 
-    email: Mapped[str] = mapped_column(String(100), unique = True, index = True)
-    telegram_id: Mapped[int | None] = mapped_column(BigInteger, index = True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, index = True, unique=True)
+    username: Mapped[str | None] = mapped_column(index = True)
     first_name: Mapped[str | None] = mapped_column(String(50))
-    password: Mapped[str] = mapped_column(String(255))
+    last_name: Mapped[str | None] = mapped_column(String(50))
 
-    is_staff: Mapped[bool] = mapped_column(default = False)
-    is_superuser: Mapped[bool] = mapped_column(default = False)
+    role: Mapped[Role] = mapped_column(default=Role.user)
     for_free: Mapped[bool] = mapped_column(default = False)
     ban: Mapped[bool] = mapped_column(default = False)
 
     mexc_api_key: Mapped[str | None] = mapped_column(String(100))
     mexc_secret_key: Mapped[str | None] = mapped_column(String(150))
+
+    deleted: Mapped[bool] = mapped_column(default = False)
+    
